bcc8885eb11ae1e6833e39d471f04e4684690c2a
Terence Parr <parrt@antlr.org>
2015-04-02 15:29:54 -0700

    add more code

---
+++ src/smalltalk/vm/SystemDictionary.java
@@ -0,0 +1,59 @@
+package smalltalk.vm;
+
+import org.antlr.symtab.Symbol;
+import smalltalk.compiler.STClass;
+import smalltalk.compiler.STMethod;
+import smalltalk.compiler.STSymbolTable;
+import smalltalk.vm.primitive.STMetaClassObject;
+import smalltalk.vm.primitive.STObject;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/** Stores predefined system objects like nil, true, false, Transcript as well
+ *  as all {@link smalltalk.vm.primitive.STMetaClassObject}'s.  You should
+ *  create just one instance of STNil for nil and an instance of STBoolean
+ *  for true and for false.
+ */
+public class SystemDictionary {
+	// All metaclass info and any predefined global objects like nil, true, ...
+	protected final Map<String,STObject> objects = new LinkedHashMap<>();
+
+	public final VirtualMachine vm;
+
+	public SystemDictionary(VirtualMachine vm) {
+		this.vm = vm;
+	}
+
+	/** Convert the symbol table with classes, methods, and compiled code
+	 *  (as computed by the compiler) into a system dictionary that has
+	 *  meta-objects as well as the necessary predefined objects
+	 *  such as nil.
+	 *
+	 *  This method assumes that the compiler has augmented the symbol table
+	 *  symbols such as {@link STMethod} with pointers to the
+	 *  {@link smalltalk.vm.primitive.STCompiledBlock}s.
+	 */
+	public void symtabToSystemDictionary(STSymbolTable symtab) {
+	}
+
+	/** Define predefined objects nil, true, false, Transcript. */
+	public void initPredefinedObjects() {
+	}
+
+	public STObject lookup(String id) {
+		return null;
+	}
+
+	public STMetaClassObject lookupClass(String id) {
+		return null;
+	}
+
+	public void defineMetaObject(String name, STMetaClassObject meta) {
+	}
+
+	public Collection<STObject> getObjects() { return null; }
+
+	public void define(String id, STObject v) { }
+}
---
+++ src/smalltalk/vm/VirtualMachine.java
@@ -0,0 +1,221 @@
+package smalltalk.vm;
+
+import smalltalk.compiler.STSymbolTable;
+import smalltalk.vm.exceptions.BlockCannotReturn;
+import smalltalk.vm.exceptions.IndexOutOfRange;
+import smalltalk.vm.exceptions.InternalVMException;
+import smalltalk.vm.exceptions.MessageNotUnderstood;
+import smalltalk.vm.exceptions.MismatchedBlockArg;
+import smalltalk.vm.exceptions.StackUnderflow;
+import smalltalk.vm.exceptions.TypeError;
+import smalltalk.vm.exceptions.UndefinedGlobal;
+import smalltalk.vm.exceptions.UnknownClass;
+import smalltalk.vm.exceptions.UnknownField;
+import smalltalk.vm.exceptions.VMException;
+import smalltalk.vm.primitive.BlockContext;
+import smalltalk.vm.primitive.BlockDescriptor;
+import smalltalk.vm.primitive.MethodContext;
+import smalltalk.vm.primitive.ObjectPool;
+import smalltalk.vm.primitive.Primitive;
+import smalltalk.vm.primitive.STArray;
+import smalltalk.vm.primitive.STBoolean;
+import smalltalk.vm.primitive.STCompiledBlock;
+import smalltalk.vm.primitive.STFloat;
+import smalltalk.vm.primitive.STInteger;
+import smalltalk.vm.primitive.STMetaClassObject;
+import smalltalk.vm.primitive.STNil;
+import smalltalk.vm.primitive.STObject;
+import smalltalk.vm.primitive.STString;
+
+/** A VM for a subset of Smalltalk.
+ *
+ *  3 HUGE simplicity factors in this implementation: we ignore GC,
+ *  efficiency, and don't expose execution contexts to smalltalk programmers.
+ *
+ *  Because of the shared {@link SystemDictionary#objects} list (ThreadLocal)
+ *  in SystemDictionary, each VirtualMachine must run in its own thread
+ *  if you want multiple.
+ */
+public class VirtualMachine {
+	/** The dictionary of global objects including class meta objects */
+	public final SystemDictionary systemDict; // singleton
+
+	/** "This is the active context itself. It is either a MethodContext
+	 *  or a BlockContext." BlueBook p 605 in pdf.
+	 */
+	public MethodContext ctx;
+
+	/** Trace instructions and show stack during exec? */
+	public boolean trace = false;
+
+	public VirtualMachine(STSymbolTable symtab) {
+		// create system dictionary and predefined nil, true, false, Transcript
+		// convert symbol table ClassSymbols to STMetaClassObjects
+	}
+
+	/** look up MainClass>>main and execute it */
+	public STObject execMain() {
+		// ...
+		return exec(mainObject,main);
+	}
+
+	/** Begin execution of the bytecodes in method relative to a receiver
+	 *  (self) and within a particular VM. exec() creates an initial
+	 *  method context to simulate a call to the method passed in.
+	 *
+	 *  Return the value left on the stack after invoking the method,
+	 *  or return self/receiver if there's nothing on the stack.
+	 */
+	public STObject exec(STObject self, STCompiledBlock method) {
+		ctx = null;
+		MethodContext initialContext = new MethodContext(this, method, self);
+		pushContext(initialContext);
+		// fetch-decode-execute loop
+		while ( true ) {
+			if ( trace ) traceInstr(); // show instr first then stack after to show results
+			int op = 0; // ...
+			switch ( op ) {
+				case Bytecode.NIL:
+					break;
+			}
+			if ( trace ) traceStack(); // show stack *after* execution
+		}
+		return ctx!=null ? ctx.receiver : null;
+	}
+
+	public void error(String type, String msg) throws VMException {
+		error(type, null, msg);
+	}
+
+	public void error(String type, Exception e, String msg) throws VMException {
+		String stack = getVMStackString();
+		switch ( type ) {
+			case "MessageNotUnderstood":
+				throw new MessageNotUnderstood(msg,stack);
+			case "IndexOutOfRange":
+				throw new IndexOutOfRange(msg,stack);
+			case "BlockCannotReturn":
+				throw new BlockCannotReturn(msg,stack);
+			case "StackUnderflow":
+				throw new StackUnderflow(msg,stack);
+			case "UndefinedGlobal":
+				throw new UndefinedGlobal(msg,stack);
+			case "MismatchedBlockArg":
+				throw new MismatchedBlockArg(msg,stack);
+			case "InternalVMException":
+				throw new InternalVMException(e,msg,stack);
+			case "UnknownClass":
+				throw new UnknownClass(msg,stack);
+			case "TypeError":
+				throw new TypeError(msg,stack);
+			case "UnknownField":
+				throw new UnknownField(msg,stack);
+			default :
+				throw new VMException(msg,stack);
+		}
+	}
+
+	public void error(String msg) throws VMException {
+		error("unknown", msg);
+	}
+
+	public void pushContext(MethodContext ctx) {
+		ctx.invokingContext = this.ctx;
+		this.ctx = ctx;
+	}
+
+	public void popContext() { ctx = ctx.invokingContext; }
+
+	public static STObject TranscriptStream_SHOW(MethodContext ctx, int nArgs, Primitive primitive) {
+		VirtualMachine vm = ctx.vm;
+		// ...
+		return null;  // leave receiver for primitive methods
+	}
+
+	public STMetaClassObject lookupClass(String id) {
+		return systemDict.lookupClass(id);
+	}
+
+	// D e b u g g i n g
+
+	void trace() {
+		traceInstr();
+		traceStack();
+	}
+
+	void traceInstr() {
+		String instr = Bytecode.disassembleInstruction(ctx.method, ctx.ip);
+		System.out.printf("%-40s", instr);
+	}
+
+	void traceStack() {
+		MethodContext c = ctx;
+		STMetaClassObject enclosingClass = c.method.enclosingClass;
+		String s;
+		if ( enclosingClass!=null ) {
+			s = enclosingClass.name + ">>" + c.method.name + pLocals(c) + pContextWorkStack(c);
+		}
+		else {
+			s = c.method.qualifiedName + pLocals(c) + pContextWorkStack(c);
+		}
+		System.out.println(s);
+	}
+
+	public String getVMStackString() {
+		StringBuilder stack = new StringBuilder();
+		MethodContext c = ctx;
+		while ( c!=null ) {
+			int ip = c.prev_ip;
+			if ( ip<0 ) ip = c.ip;
+			String instr = Bytecode.disassembleInstruction(c.method, ip);
+			String location = c.currentFile+":"+c.currentLine+":"+c.currentCharPos;
+			String mctx = c.method.qualifiedName + pLocals(c) + pContextWorkStack(c);
+			String s = String.format("    at %50s%-20s executing %s\n",
+									 mctx,
+									 String.format("(%s)",location),
+									 instr);
+			stack.append(s);
+			c = c.invokingContext;
+		}
+		return stack.toString();
+	}
+
+	String pContextWorkStack(MethodContext ctx) {
+		StringBuilder buf = new StringBuilder();
+		buf.append("[");
+		for (int i=0; i<=ctx.sp; i++) {
+			if ( i>0 ) buf.append(", ");
+			pValue(buf, ctx.stack[i]);
+		}
+		buf.append("]");
+		return buf.toString();
+	}
+
+	String pLocals(MethodContext ctx) {
+		StringBuilder buf = new StringBuilder();
+		buf.append("[");
+		for (int i=0; i<ctx.locals.length; i++) {
+			if ( i>0 ) buf.append(", ");
+			pValue(buf, ctx.locals[i]);
+		}
+		buf.append("]");
+		return buf.toString();
+	}
+
+	void pValue(StringBuilder buf, STObject v) {
+		if ( v==null ) buf.append("null");
+		else if ( v==nil() ) buf.append("nil");
+		else if ( v instanceof STString) buf.append("'"+v.asString()+"'");
+		else if ( v instanceof BlockDescriptor) {
+			BlockDescriptor blk = (BlockDescriptor) v;
+			buf.append(blk.block.name);
+		}
+		else if ( v instanceof STMetaClassObject ) {
+			buf.append(v.toString());
+		}
+		else {
+			STObject r = v.asString(); //getAsString(v);
+			buf.append(r.toString());
+		}
+	}
+}
